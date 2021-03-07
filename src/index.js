85b47bd381539e61626c275bc013c568624ce100
Yuristy <yoosambee@gmail.com>
2021-03-07 17:13:01 +0300

    Finished the code.

--- src/index.js
+++ src/index.js
@@ -46,14 +46,15 @@ class Game extends React.Component {
     super(props);
     this.state = {
       history: [{
-          squares: Array(9).fill(null),
+        squares: Array(9).fill(null),
         }],
+      stepNumber: 0,
       xIsNext: true,
     }
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -64,13 +65,21 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
-    xIsNext: !this.state.xIsNext,  
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,  
   });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    })
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -79,7 +88,7 @@ class Game extends React.Component {
       'К началу игры';
 
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
